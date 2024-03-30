384cf9b241d67fd8066988782666a7e4058dffaf
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-03-30 11:12:50 -0400

    new features

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #bash
-docker buildx build --platform=linux/arm64,linux/amd64 --push --tag maxiplux/api-gate-way:1.0.0 -f ./Dockerfile .
+docker buildx build --platform=linux/arm64,linux/amd64 --push --tag maxiplux/eureka-server:1.0.0 -f ./Dockerfile .
--- src/main/java/app/quantun/math/MathApplication.java
+++ src/main/java/app/quantun/math/MathApplication.java
@@ -2,8 +2,10 @@ package app.quantun.math;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MathApplication {
 
     public static void main(String[] args) {
