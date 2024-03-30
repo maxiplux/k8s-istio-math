e63a264a700940264afc847ef2843d6abdbcb2c3
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-03-30 11:24:31 -0400

    new features

--- src/test/java/app/quantun/math/MathApplicationTests.java
+++ src/test/java/app/quantun/math/MathApplicationTests.java
@@ -3,10 +3,10 @@ package app.quantun.math;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MathApplicationTests {
 
-    @Test
+  //  @Test
     void contextLoads() {
     }
 
