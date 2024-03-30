54e36a49ab54c25f5748186e9c3f7d450a2ec234
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-03-30 14:51:40 -0400

    new features

--- src/main/java/app/quantun/math/MathApplication.java
+++ src/main/java/app/quantun/math/MathApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class MathApplication {
 
     public static void main(String[] args) {
