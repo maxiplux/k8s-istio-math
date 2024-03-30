d060bd9bf0e0af2f2eb18ec04269cdb82a98beb5
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-03-29 23:58:01 -0400

    Math 2

---
+++ deployer.sh
@@ -0,0 +1,4 @@
+CURRENT=${PWD}
+cd ${CURRENT}/k8s && kubectl apply -f  namespace.yml
+cd ${CURRENT}/k8s/math-add-subtract && kubectl apply -f .
+cd ${CURRENT}/k8s/math-division-multiplication && kubectl apply -f .
--- k8s/tester.py
+++ k8s/tester.py
@@ -1,19 +1,47 @@
 import requests
 import random
+from multiprocessing import Process
+
+
 def calll_api(server="localhost",services="division",a=5, b=7,port=31264):
+
     url = f"http://{server}:{port}/api/v1/math/{services}?a={a}&b={b}"
     payload={}
     headers = {}
     response = requests.request("POST", url, headers=headers, data=payload)
-    return response.text
+    value=response.text
 
-for i in range (0,1000):
-    random_number = random.randint(0,500)
-    server="localhost"
-    port=31264
-    print (calll_api(server=server,services="multiplication",a=random_number,b=random_number,port=port))
-    print (calll_api(server=server,services="division",a=random_number,b=random.randint(0,1),port=port))
+    print (f"Server: {server}, Port: {port}, Service: {services}, a: {a}, b: {b}, Response: {value}")
 
-    port=32342
-    print (calll_api(server=server,services="add",a=random_number,b=random_number,port=port))
-    print (calll_api(server=server,services="subtract",a=random_number*random_number,b=random_number,port=port))
+    return value
+def main():
+    processes = []
+    for i in range (0,1000):
+        random_number = random.randint(0,500)
+        server="localhost"
+        port=31264
+        port2=32342
+
+        calls = [
+            (server,"multiplication",random_number,random.randint(0,500),port),
+            (server,"division",random_number,random.randint(0,1),port),
+
+            (server,"subtract",random_number,random.randint(0,500),port2),
+            (server,"add",random_number,random.randint(0,1),port2),
+        ]
+
+
+
+
+        for call in calls:
+            p = Process(target=calll_api, args=call)
+            p.start()
+            processes.append(p)
+
+        # Wait for all processes to finish
+    for p in processes:
+        p.join()
+
+
+if __name__ == '__main__':
+    main()
