411cc63f546de2974caa9b5a37d4d3c6fd68f534
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-03-30 00:33:30 -0400

    new features

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform=linux/arm64,linux/amd64 --push --tag maxiplux/math-add-subtract:1.2.0 -f ./Dockerfile .
+docker buildx build --platform=linux/arm64,linux/amd64 --push --tag maxiplux/math-add-subtract:1.3.0 -f ./Dockerfile .
