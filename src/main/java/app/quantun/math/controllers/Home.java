dc8555548f24dd66fe947ffacde035f745866f69
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-03-29 22:00:36 -0400

    Math 2

--- src/main/java/app/quantun/math/controllers/Home.java
+++ src/main/java/app/quantun/math/controllers/Home.java
@@ -1,5 +1,8 @@
 package app.quantun.math.controllers;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,35 +11,73 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/api/v1/math")
 @Slf4j
 public class Home {
 
-        @RequestMapping(value = "/multiplication", method = RequestMethod.POST)
+    private Counter hitsCounterToMultiplication;
+    private Counter hitsCounterToDivicion;
 
-        public Double multiplication(@RequestParam( required = true,  name = "a") Double a, @RequestParam(required = true,  name = "b") Double b) {
-            if (a == null || b == null) {
-                log.error("Invalid input" );
-                return 0.0;
-            }
 
-            return a * b;
+    public Home(MeterRegistry registry) {
+        hitsCounterToMultiplication = Counter.builder("hitsCounterToMultiplication")
+                .description("Number of hits  to the multiplication")
+                .register(registry);
+        hitsCounterToDivicion = Counter.builder("hitsCounterTodivision")
+                .description("Number of hits  to  division")
+                .register(registry);
+    }
+
+
+    @RequestMapping(value = "/multiplication", method = RequestMethod.POST)
+    @Timed(value = "multiplication", longTask = true)
+    public Double multiplication(@RequestParam(required = true, name = "a") Double a, @RequestParam(required = true, name = "b") Double b) {
+        this.sleep();
+        if (a == null || b == null) {
+            log.error("Invalid input");
+            return 0.0;
         }
+
+        return a * b;
+    }
+
     //division and multiplication
     @RequestMapping(value = "/division", method = RequestMethod.POST)
+    @Timed(value = "division", longTask = true)
     public ResponseEntity<?> division(@RequestParam("a") Double a, @RequestParam("b") Double b) {
+        this.sleep();
 
         if (a == null || b == null) {
-            log.error("Invalid input" );
+            log.error("Invalid input");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input A or B is null");
         }
         if (b == 0) {
-            log.error("Invalid input" );
+            log.error("Invalid input");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid input B");
         }
 
-        return  ResponseEntity.ok(a / b);
+        return ResponseEntity.ok(a / b);
+    }
+
+    public void sleep() {
+        Random random = new Random();
+
+
+        int sleepTime = 1 + random.nextInt(7);
+
+        log.info("Sleeping for " + sleepTime + " seconds...");
+
+        try {
+            // Convert seconds to milliseconds and sleep
+            Thread.sleep(sleepTime * 1000L);
+        } catch (InterruptedException e) {
+            log.error("Sleep was interrupted");
+        }
+
+        log.info("Woke up!");
     }
 
 }
--- to-docker-hub.sh
+++
@@ -1,2 +0,0 @@
-#bash
-docker buildx build --platform=linux/arm64,linux/amd64 --push --tag maxiplux/math-add-subtract:1.0.0 -f ./Dockerfile .
