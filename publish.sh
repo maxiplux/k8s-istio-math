5a82b9cf52a698c71ebf6232ac5fb738a79bdb9a
Juan Francisco Mosquera <maxiplux@gmail.com>
2024-03-30 20:21:08 -0400

    new features

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #bash
-docker buildx build --platform=linux/arm64,linux/amd64 --push --tag maxiplux/api-gate-way:1.6.0 -f ./Dockerfile .
+docker buildx build --platform=linux/arm64,linux/amd64 --push --tag maxiplux/api-gate-way:1.6.1 -f ./Dockerfile .
